93b29abb9055b35882297cc7cc3aef4ad42e8b42
Curtabug <148663845+Curtabug@users.noreply.github.com>
2023-10-22 09:28:14 -0400

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
 equation = 0
 num1 = input("Please input a number. ")
-operator_input = input("Please input an operation. A for Add, S for subtract, M for multiply, D for divide, and E for exponent. ")
+operator_input = input("Please input an operation. Put the first letter of the operation.")
 num2 = input("Please input a second number. ")
 
 print(f"{num1} {operator_input} {num2}")
