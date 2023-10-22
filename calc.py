da0f9db0cf3930f409c3c8de60ce6e49698fd043
Curtabug <148663845+Curtabug@users.noreply.github.com>
2023-10-22 09:26:48 -0400

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,1 +1,6 @@
-print("Hello World!")
+equation = 0
+num1 = input("Please input a number. ")
+operator_input = input("Please input an operation. A for Add, S for subtract, M for multiply, D for divide, and E for exponent. ")
+num2 = input("Please input a second number. ")
+
+print(f"{num1} {operator_input} {num2}")
